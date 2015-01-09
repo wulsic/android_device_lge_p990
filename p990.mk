c3707d56b4196c7c0635b779291430a01cbc9949
Wulsic <wulsic@hotmail.com>
2015-01-09 21:50:42 +0100

    Add media codecs back

--- p990.mk
+++ p990.mk
@@ -21,8 +21,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/asound.conf:system/etc/asound.conf \
     $(LOCAL_PATH)/config/audio_policy.conf:system/etc/audio_policy.conf
 
-# media files
+# Media files
 PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     $(LOCAL_PATH)/media_profiles.xml:system/etc/media_profiles.xml \
     $(LOCAL_PATH)/media_codecs.xml:system/etc/media_codecs.xml
 
